9b80eabaddfcc1d7ef6c848af493e61078a2eb42
Salma-Engmelb <docmel2409@gmail.com>
2024-08-04 09:58:14 +0000

    Add tests for subtract function

---
+++ tests/test_subtract.py
@@ -0,0 +1,21 @@
+# tests/test_subtract.py
+
+import unittest
+from operations.subtract import subtract
+
+class TestSubtract(unittest.TestCase):
+    """
+    Contains unit tests for the subtract function.
+    """
+
+    def test_subtract(self):
+        """
+        Tests the subtract function with various inputs.
+        """
+        self.assertEqual(subtract(10, 5), 5, "Should be 5")
+        self.assertEqual(subtract(-1, 1), -2, "Should be -2")
+        self.assertEqual(subtract(-1, -1), 0, "Should be 0")
+        self.assertEqual(subtract(0, 0), 0, "Should be 0")
+
+if __name__ == '__main__':
+    unittest.main()
