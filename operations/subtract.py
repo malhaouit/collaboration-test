58908edb48510b794356fbda3e84e19f7a9c73bd
Salma-Engmelb <docmel2409@gmail.com>
2024-08-04 10:24:17 +0000

    Add subtract function and test

---
+++ operations/subtract.py
@@ -0,0 +1,14 @@
+# operations/subtract.py
+
+def subtract(x, y):
+    """
+    Subtracts the second number from the first number.
+
+    Parameters:
+    x (float): The number from which y will be subtracted.
+    y (float): The number to subtract from x.
+
+    Returns:
+    float: The result of x - y.
+    """
+    return x - y
