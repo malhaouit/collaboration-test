be1b335704e3e3ec529a5d1b78a337e71651cc13
sanzamcmillian <g18s2658@ru.ac.za>
2024-08-09 18:26:59 +0200

    square

--- operations/sqrt.py
+++ operations/sqrt.py
@@ -2,5 +2,8 @@
 """Module to compute square root"""
 
 
-def sqrt():
+def sqrt(num: float) -> float:
     """function to compute square root"""
+    if num < 0:
+        return ValueError('number must be greater than 0')
+    return (num ** 0.5)
